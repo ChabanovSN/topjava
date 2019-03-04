ae1d4fc8221891297253973b7f835ff7a6bf9f93
Chabanov Sergay <chabanovsn@mail.ru>
2019-03-04 10:37:08 +0300

    lesson 4 hw04

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -14,7 +15,6 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 @Service
 public class UserServiceImpl implements UserService {
 
-
     private final UserRepository repository;
 
     @Autowired
@@ -24,6 +24,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(User user) {
+        Assert.notNull(user, "user must not be null");
         return repository.save(user);
     }
 
@@ -39,6 +40,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getByEmail(String email) throws NotFoundException {
+        Assert.notNull(email, "email must not be null");
         return checkNotFound(repository.getByEmail(email), "email=" + email);
     }
 
@@ -49,6 +51,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void update(User user) {
+        Assert.notNull(user, "user must not be null");
         checkNotFoundWithId(repository.save(user), user.getId());
     }
 }
