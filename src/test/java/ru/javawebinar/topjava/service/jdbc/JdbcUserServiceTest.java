94f3f847ca80258a3414270ccf729713a7cd57f2
Chabanov Sergay <chabanovsn@mail.ru>
2019-04-03 18:07:09 +0300

    hw06

---
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -0,0 +1,18 @@
+package ru.javawebinar.topjava.service.jdbc;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+import static ru.javawebinar.topjava.Profiles.JDBC;
+
+@ActiveProfiles(JDBC)
+public class JdbcUserServiceTest extends AbstractUserServiceTest {
+    @Ignore
+    @Test
+    @Override
+    public void testValidation() throws Exception {
+        super.testValidation();
+    }
+}
